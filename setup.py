868f315e8bd56cdf26d62e93925618152baf9d30
VSV170314 <159541564+VSV170314@users.noreply.github.com>
2024-03-13 18:49:08 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
 
 VERSION = '0.0.1'
 DESCRIPTION = 'Helps reduce code'
-LONG_DESCRIPTION = 'A package that allows to reduce the lines of code some are already in the classes'
 
 # Setting up
 setup(
@@ -19,12 +18,11 @@ setup(
     author_email="<mail@vishwa170314.com>",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
-    long_description=long_description,
     packages=find_packages(),
-    install_requires=['opencv-python', 'pyautogui', 'pyaudio'],
+    install_requires=[],
     keywords=['python', 'code', 'reduce', 'code reduce', "work_less", "less_code", "clean_code"],
     classifiers=[
-        "Development Status :: 1 - Planning",
+        "Development Status :: 1 - releasing",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "Operating System :: Unix",
