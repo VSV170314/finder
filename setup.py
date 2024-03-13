d5b1f32c5b1c4fc10c60312d4071d168d85e6ab5
VSV170314 <159541564+VSV170314@users.noreply.github.com>
2024-03-13 19:15:34 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,18 +15,5 @@ setup(
     name="finder-vsv170314",
     version=VERSION,
     author="VSV170314 (Vishwa S Vikram)",
-    author_email="<mail@vishwa170314.com>",
-    description=DESCRIPTION,
-    long_description_content_type="text/markdown",
-    packages=find_packages(),
     install_requires=[],
-    keywords=['python', 'code', 'reduce', 'code reduce', "work_less", "less_code", "clean_code"],
-    classifiers=[
-        "Development Status :: 1 - releasing",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3",
-        "Operating System :: Unix",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-    ]
 )
